c027f6f4469437834ee603d048f0b426e0c3b6ab
sneha.busireddy <sneha.busireddy@TRI03L-PF2RMHV2>
2021-10-26 18:05:08 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Demo_Async_Await
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Demo Async<Task> and Await<Task>");
+            Method1();
+            Method2();
+            Console.ReadKey();
+        }
+        public static async Task Method1()
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.WriteLine(" Method 1");
+                    // Do something
+                    Task.Delay(100).Wait();
+                }
+            });
+        }
+
+
+        public static void Method2()
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                Console.WriteLine(" Method 2");
+                // Do something
+                Task.Delay(100).Wait();
+            }
+        }
+    }
+}
